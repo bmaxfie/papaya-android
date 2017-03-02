74c29adf9ba9bfdaf421aff4af00946bb93a7efa
Adam Johnston <johnst63@purdue.edu>
2017-03-01 23:50:45 -0500

    Added PreferenceHandler class

---
+++ papaya/papaya/app/src/main/java/com/papaya/scotthanberg/papaya/PreferenceHandler.java
@@ -0,0 +1,42 @@
+package com.papaya.scotthanberg.papaya;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by Sheolfire on 3/1/2017.
+ */
+
+public class PreferenceHandler extends Fragment {
+    private SharedPreferences sharedPref;
+
+    public PreferenceHandler() {
+        Context context = getActivity();
+        sharedPref = context.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+    }
+
+    /**
+     * Stores a key value pair in the SharedPreferences file
+     * @param key the key to be associated with the value
+     * @param value the value key will point to
+     * @return whether or not the write succeeded (whether or not key was null)
+     */
+    public boolean writeToSharedPref(String key, String value) {
+        if(key == null)
+            return false;
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(key, value);
+        editor.commit();
+        return true;
+    }
+
+    /**
+     * Obtains the value associated with the key
+     * @param key the key to get the value of
+     * @return
+     */
+    public String readFromSharedPref(String key) {
+        return sharedPref.getString(key, "Value does not exist.");
+    }
+}
