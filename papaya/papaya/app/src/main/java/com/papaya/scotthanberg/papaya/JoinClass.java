888c9e4a4d13ae7b2d953fbc9067ae2271e54004
Caleb Flynn <flynn37@purdue.edu>
2017-03-03 13:38:43 -0500

    add joinClass

---
+++ papaya/papaya/app/src/main/java/com/papaya/scotthanberg/papaya/JoinClass.java
@@ -0,0 +1,13 @@
+package com.papaya.scotthanberg.papaya;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JoinClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_class);
+    }
+}
