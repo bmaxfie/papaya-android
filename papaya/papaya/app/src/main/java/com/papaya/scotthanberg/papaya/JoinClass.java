41648a45431cf43dc100d746f2fe8bb43a4a6a22
Caleb Flynn <flynn37@purdue.edu>
2017-03-03 14:31:12 -0500

    fix JoinClass

--- papaya/papaya/app/src/main/java/com/papaya/scotthanberg/papaya/JoinClass.java
+++ papaya/papaya/app/src/main/java/com/papaya/scotthanberg/papaya/JoinClass.java
@@ -4,18 +4,59 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.HorizontalScrollView;
+import android.widget.RelativeLayout;
+
+import static com.papaya.scotthanberg.papaya.R.id.dropDown;
 
 public class JoinClass extends AppCompatActivity {
+
+    //Main Menu Buttons
+    private RelativeLayout dropDown;
+    private View backdrop;
+    private HorizontalScrollView horizontalScroll;
+    private Button newStudySession, sortByClass, manageClasses, findFriends, joinNewClass;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_class);
+        dropDown = (RelativeLayout) findViewById(R.id.dropDown);
+        newStudySession = (Button) findViewById(R.id.NewStudySession);
+        sortByClass = (Button) findViewById(R.id.SortByClass);
+        manageClasses = (Button) findViewById(R.id.ManageClasses);
+        findFriends = (Button) findViewById(R.id.FindFriends);
+        joinNewClass = (Button) findViewById(R.id.JoinNewClass);;
     }
 
     public void backToHome(View view) {
         Intent home = new Intent(this, HomeScreen.class);
         home.putExtra("from", "JoinClass");
         startActivity(home);
+    }
+
+    public void openMenu(View view) {
+        if (dropDown.getVisibility()==View.VISIBLE) {
+            dropDown.setVisibility(View.GONE);
+            System.out.println("THIS IS WORKING");
+            //   horizontalScroll.setVisibility(View.VISIBLE);
+            //  backdrop.setVisibility(View.VISIBLE);
+            newStudySession.setVisibility(View.GONE);
+            sortByClass.setVisibility(View.GONE);
+            manageClasses.setVisibility(View.GONE);
+            findFriends.setVisibility(View.GONE);
+            joinNewClass.setVisibility(View.GONE);
+        } else {
+            dropDown.setVisibility(View.VISIBLE);
+            // backdrop.setVisibility(View.GONE);
+            // horizontalScroll.setVisibility(View.GONE);
+            newStudySession.setVisibility(View.VISIBLE);
+            sortByClass.setVisibility(View.VISIBLE);
+            manageClasses.setVisibility(View.VISIBLE);
+            findFriends.setVisibility(View.VISIBLE);
+            joinNewClass.setVisibility(View.VISIBLE);
+        }
     }
 }
