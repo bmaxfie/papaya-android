ebe2717b000ca295c625d57dbda8fcd7d04373f1
scotthanberg <shanberg@purdue.edu>
2017-03-31 18:18:24 -0400

    It works!

--- papaya/papaya/app/src/main/java/com/papaya/scotthanberg/papaya/SessionMarkerDialog.java
+++ papaya/papaya/app/src/main/java/com/papaya/scotthanberg/papaya/SessionMarkerDialog.java
@@ -6,6 +6,16 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
+
 /**
  * Created by scotthanberg on 3/31/17.
  */
@@ -27,7 +37,7 @@ public class SessionMarkerDialog extends DialogFragment {
         builder.setMessage("Join this study session?")
                 .setPositiveButton("Join", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                     //   addUserToSession(sessionId);
+                        addUserToSession(sessionId);
                     }
                 })
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -38,19 +48,24 @@ public class SessionMarkerDialog extends DialogFragment {
         // Create the AlertDialog object and return it
         return builder.create();
     }
-    /*
+
     public void addUserToSession(String sessionId) {
+        // I hardcoded my user_id only because we're going to have to change it anyway with the new Data Manager
+        JSONObject info = new JSONObject();
+        try {
+            info.put("user_id", "bNvqxLf+m6VbMx1x8OCQrw==");
+            info.put("authentication_key",GPlusFragment.getAuthentication_key());
+            info.put("service", GPlusFragment.getService());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        System.out.println(sessionId);
         String url = "https://a1ii3mxcs8.execute-api.us-west-2.amazonaws.com/Beta/classes/" + "111" + "/sessions/" + sessionId;
         JsonObjectRequest jsObjRequest = new JsonObjectRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                (Request.Method.POST, url, info, new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-
-                        try {
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
+                        System.out.println(response);
                     }
                 }, new Response.ErrorListener() {
 
@@ -64,7 +79,7 @@ public class SessionMarkerDialog extends DialogFragment {
         MySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsObjRequest);
 
     }
-    */
+
 
     public void setSessionId(String title) {
         sessionId = title;
