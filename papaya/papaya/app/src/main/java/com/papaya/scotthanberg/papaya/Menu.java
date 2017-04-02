0c3ef6dbf67ee626c7a86a53e891400f8c97d8d7
Caleb Flynn <flynn37@purdue.edu>
2017-04-02 17:36:20 -0400

    menu can go anywhere with little code

--- papaya/papaya/app/src/main/java/com/papaya/scotthanberg/papaya/Menu.java
+++ papaya/papaya/app/src/main/java/com/papaya/scotthanberg/papaya/Menu.java
@@ -2,8 +2,10 @@ package com.papaya.scotthanberg.papaya;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
@@ -21,29 +23,74 @@ public class Menu extends AppCompatActivity {
     //Main Menu Buttons
     private ImageView menubutton;
     private TextView textView;
-    private RelativeLayout dropDown;
+    private RelativeLayout dropDown, title;
     private View backdrop;
     private HorizontalScrollView horizontalScroll;
     private Button newStudySession, sortByClass, manageClasses, findFriends, joinNewClass;
-    Context context;
+    private Context context;
 
     public Menu(Context context) {
         this.context = context;
-        View view = ((Activity) context).getWindow().getDecorView().findViewById(android.R.id.content);
 
-        //get the view of the <include> so that it doesnt break
-        view = (View) ((View) view.getParent()).getParent();
+        final ImageView menuButton = (ImageView) ((Activity)context).findViewById(R.id.menuButton);
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                toggleMenu(v.getContext());
+            }
+        });
+
+        final Button newStudySessionButton = (Button) ((Activity)context).findViewById(R.id.NewStudySession);
+        newStudySessionButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                toggleMenu(v.getContext());
+            }
+        });
+
+        final Button sortByClassButton = (Button) ((Activity)context).findViewById(R.id.SortByClass);
+        sortByClassButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                toggleMenu(v.getContext());
+            }
+        });
+
+        final Button manageClassesButton = (Button) ((Activity)context).findViewById(R.id.ManageClasses);
+        manageClassesButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                toggleMenu(v.getContext());
+            }
+        });
+
+        final Button myFriendsButton = (Button) ((Activity)context).findViewById(R.id.FindFriends);
+        myFriendsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                buttonMyFriends(v);
+            }
+        });
+
+        final Button joinNewClassButton = (Button) ((Activity)context).findViewById(R.id.JoinNewClass);
+        joinNewClassButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                buttonJoinClass(v);
+            }
+        });
+
+
+
+
         //inialzie variables
-        dropDown = (RelativeLayout) view.findViewById(R.id.dropDown);
-        //horizontalScroll = (HorizontalScrollView) view.findViewById(R.id.horizontalScroll);
-        //backdrop = (View) view.findViewById(R.id.horizontalBackdrop);
-        view = (View) dropDown;
+        dropDown = (RelativeLayout) ((Activity)context).findViewById(R.id.dropDown);
+        //horizontalScroll = (HorizontalScrollView) ((Activity)context).findViewById(R.id.horizontalScroll);
+        //backdrop = (View) ((Activity)context).findViewById(R.id.horizontalBackdrop);
+
 
         newStudySession = (Button) ((Activity)context).findViewById(R.id.NewStudySession);
         sortByClass = (Button) ((Activity)context).findViewById(R.id.SortByClass);
         manageClasses = (Button) ((Activity)context).findViewById(R.id.ManageClasses);
         findFriends = (Button) ((Activity)context).findViewById(R.id.FindFriends);
         joinNewClass = (Button) ((Activity)context).findViewById(R.id.JoinNewClass);
+
+        title = (RelativeLayout) ((Activity)context).findViewById(R.id.title);
+
     }
     public void toggleMenu(Context context) {
         //get the view of the <include> so that it doesnt break
@@ -67,23 +114,30 @@ public class Menu extends AppCompatActivity {
 
         } else {
             dropDown.setVisibility(View.VISIBLE);
-            dropDown.bringToFront();
+            //dropDown.bringToFront();
             //backdrop.setVisibility(View.GONE);
             //horizontalScroll.setVisibility(View.GONE);
             newStudySession.setVisibility(View.VISIBLE);
-            newStudySession.bringToFront();
+            //newStudySession.bringToFront();
             sortByClass.setVisibility(View.VISIBLE);
-            sortByClass.bringToFront();
+            //sortByClass.bringToFront();
             manageClasses.setVisibility(View.VISIBLE);
-            manageClasses.bringToFront();
+            //manageClasses.bringToFront();
             findFriends.setVisibility(View.VISIBLE);
-            findFriends.bringToFront();
+            //findFriends.bringToFront();
             joinNewClass.setVisibility(View.VISIBLE);
-            joinNewClass.bringToFront();
-//            final float scale = context.getResources().getDisplayMetrics().density;
-//            int pixels = (int) (300 * scale + 0.5f);
-//            RelativeLayout rl = (RelativeLayout) ((Activity)context).findViewById(R.id.view);
-//            rl.getLayoutParams().height = pixels;
+
         }
     }
+
+    public void buttonJoinClass(View view) {
+        Intent joinClass = new Intent(this, JoinClass.class);
+        startActivity(joinClass);
+    }
+
+    public void buttonMyFriends(View view) {
+        Intent friendsList = new Intent(this, FriendsList.class);
+        startActivity(friendsList);
+    }
+
 }
