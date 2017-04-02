6318bc7ef7f9ddaa7c135189d27d53b3088c350d
Caleb Flynn <flynn37@purdue.edu>
2017-04-02 19:33:15 -0400

    scrollable description in sessionInfo

--- papaya/papaya/app/src/main/java/com/papaya/scotthanberg/papaya/SessionInfo.java
+++ papaya/papaya/app/src/main/java/com/papaya/scotthanberg/papaya/SessionInfo.java
@@ -13,14 +13,6 @@ public class SessionInfo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_session_info);
         Menu menu = new Menu(SessionInfo.this);
-        //setup menu button listener
-//        final ImageView button = (ImageView) findViewById(R.id.menuButton);
-//        button.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                Menu menu = new Menu(SessionInfo.this);
-//                menu.toggleMenu(SessionInfo.this);
-//            }
-//        });
 
         Intent studySession = getIntent(); // gets the previously created intent
 
